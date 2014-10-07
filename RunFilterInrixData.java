f0e6e25754470582129942b8c3025d13d58e425f
reidspence <reidaspence@gmail.com>
2014-10-07 14:20:36 -0400

    Revert "Added Merge and Sorting Capabilities. Still lot’s of bugs."
    This reverts commit 467d556f2d19043534c0c864f00573cae2a08898.

--- RunFilterInrixData.java
+++ RunFilterInrixData.java
@@ -1,17 +1,22 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class RunFilterInrixData {
-	
+
 	public static void main(String[] args) throws IOException{
-		//File to be analyzed
-		String filePath = "/Users/Stephen/Documents/Java/DataManagement/bin/SFPoints.csv";
-		
-		//Test Objects
-		//1MB = 1,048,576 Bytes
-		System.out.println("System Available Memory: " + InrixReader.getAvailableMemory()/1048576 + "MB");
-		
-		InrixReader.externalSort(filePath,5);
+		// TODO Auto-generated method stub
+		InrixReader IR = new InrixReader();
+		ArrayList<InrixTripLL> trips = IR.readCSV("C:/Users/SDM/Desktop/INRIX/InrixSample.csv");
+		System.out.print(trips);
+		int i = 0;
+//		for (InrixTripLL trip : trips) {
+//			if (trip.getAvgTimeDelay() < 13 || trip.getTripDuration() < 120) {
+//				trips.remove(i);
+//			}
+//			i++;
+//		}
 	}
 
 }
